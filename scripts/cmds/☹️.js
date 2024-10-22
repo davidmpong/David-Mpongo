f3a0fb96608389f40ffc20be564ee6d19a34299c
davidmpong <davimpong@gmail.com>
2024-10-22 23:28:53 +0100

    Create ☹️.js
    Signed-off-by: davidmpong <davimpong@gmail.com>

---
+++ scripts/cmds/☹️.js
@@ -0,0 +1,20 @@
+module.exports = {
+    config: {
+        name: "☹️",
+        version: "1.0",
+        author: "𝐦𝐚𝐝𝐚𝐫𝐚",
+        countDown: 5,
+        role: 0,
+        shortDescription: "commande 🦅",
+        longDescription: "commande Ok",
+        category: "reply",
+    },
+    onStart: async function(){}, 
+    onChat: async function({
+        event,
+        message,
+        getLang
+    }) {
+        if (event.body && event.body.toLowerCase() == "☹️","🙁","😟","🥺" ) return message.reply("tu mes fais pitié");
+    }
+}
