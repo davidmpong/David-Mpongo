9302a7db1620dce104eb987b33d2de8c626eab68
davidmpong <davimpong@gmail.com>
2024-10-22 23:26:02 +0100

    Create 🙂.js
    Signed-off-by: davidmpong <davimpong@gmail.com>

---
+++ scripts/cmds/🙂.js
@@ -0,0 +1,20 @@
+module.exports = {
+    config: {
+        name: "🙂",
+        version: "1.0",
+        author: "𝐦𝐚𝐝𝐚𝐫𝐚",
+        countDown: 5,
+        role: 0,
+        shortDescription: "commande ",
+        longDescription: "commande Ok",
+        category: "reply",
+    },
+    onStart: async function(){}, 
+    onChat: async function({
+        event,
+        message,
+        getLang
+    }) {
+        if (event.body && event.body.toLowerCase() == "🙂","🙃","🥲","😊") return message.reply("tu es aussi bon que mon maître David mpongo l'aigle royal 🦅___◥✇◣, ,◢✇◤__");
+    }
+}
